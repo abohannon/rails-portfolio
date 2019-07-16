03ddecb4e669772af1c13abc57d5d4b130e8585f
Adam Bohannon <adam.bohannon@housecallpro.com>
2019-07-16 11:36:23 -0700

    Integrated friendly routes for blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get "portfolio/:id", to: "portfolios#show", as: "portfolio_show"
+
   resources :blogs
 
   get "about-me", to: "pages#about"
-  get "pages/contact"
+  get "contact", to: "pages#contact"
 
   root to: 'pages#home'
 end
